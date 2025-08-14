3a9cf93561abf5e2c5ee8caa92d7d893a41218d6
Nakkana Bharathi <nakkanabharathi12@gmail.com>
2025-08-14 21:43:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Load Dialogflow Messenger
+const script = document.createElement('script');
+script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
+document.head.appendChild(script);
